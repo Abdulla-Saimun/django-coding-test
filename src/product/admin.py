156a21d137fac2daf90eeae1c0cac629214c3a93
Abdulla-Saimun <abdullah35-1921@diu.edu.bd>
2022-09-12 22:29:58 +0600

    dev

--- src/product/admin.py
+++ src/product/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Product, Variant, ProductVariant, ProductVariantPrice
 
 # Register your models here.
+class ProductAAdmin(admin.ModelAdmin):
+    list_display = ("title", "sku", "description")
+
+
+class VariantAdmin(admin.ModelAdmin):
+    list_display = ("title", "description", "active")
+
+
+class ProductVariantAdmin(admin.ModelAdmin):
+    list_display = ("variant_title", "variant", "product")
+
+
+class ProductVariantPriceAdmin(admin.ModelAdmin):
+    list_display = (
+        "product_variant_one",
+        "product_variant_two",
+        "product_variant_three",
+        "price",
+        "stock",
+        "product",
+    )
+
+
+admin.site.register(Product, ProductAAdmin)
+admin.site.register(Variant, VariantAdmin)
+admin.site.register(ProductVariant, ProductVariantAdmin)
+admin.site.register(ProductVariantPrice, ProductVariantPriceAdmin)
